02d102d88bdae430098e5ea9abe757274ce89988
superastorh <salem.m.s2030@gmail.com>
2021-03-23 14:28:00 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Router, Route, Link } from "svelte-routing";
+	import Home from "./page/Home.svelte";
+	import login from "./page/login.svelte";
+	export let url = ""; //This property is necessary declare to avoid ignore the Router
+</script>
+
+<Router url="{url}">
+	<nav>
+	   <Link to="/">Home</Link>
+	   <Link to="login">login</Link>
+	</nav>
+	<div>
+		<Route path="/"><Home /></Route>
+		<Route path="login" component="{login}" /> 
+	 </div>
+</Router>
---
+++ src/components/button.svelte
@@ -0,0 +1,4 @@
+<script>
+    export let title = "";
+</script>
+<button>{title}</button>
